3c452f613f3d3f84e37e35c6cdc2530702d7cb9b
Dev-Aaminah <aminakanwal12345@gmail.com>
2024-05-29 21:55:49 +0500

    let, const & var

---
+++ 01.js
@@ -0,0 +1,17 @@
+// Declaring a constant variable to store the student's name
+const studentName = 'Ilma';
+
+// Declaring a variable using let to store the class name
+let className = 9;
+
+// Declaring a variable using var to store the roll number
+var rollNum = 12;
+
+// Implicitly declaring a variable without any keyword (not recommended) to store the city
+city = 'Rawalpindi';
+
+// Declaring a variable using let to store whether the student is good
+let isGoodStudent = true;
+
+// Using console.table to display the variables in a table format
+console.table([studentName, className, rollNum, city, isGoodStudent]);
